3f75c9aeccaa4d0e0d79d7237f7ad7cb8ad1ab23
james <james@jamesleblanc.org>
2022-03-10 19:22:26 -0500

    Adding basic structure

--- home.php
+++ home.php
@@ -4,6 +4,15 @@
     <title>Page Title</title>
   </head>
   <body>
-    hello from the body
+    <div id='wrapper'>
+      <div>
+        <div id='tabs'>
+          <ul>
+            <li><a href='#tab1'>tab uno</a></li>
+          </ul>
+          <div id='tab1'> data for tab one </div>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
